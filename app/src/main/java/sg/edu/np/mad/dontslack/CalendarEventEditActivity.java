5b02a9376eae6b3b23c5c84968ddd5a2da71a036
sumosh <s10223863@connect.np.edu.sg>
2022-06-17 16:49:52 +0800

    layout update

--- app/src/main/java/sg/edu/np/mad/dontslack/CalendarEventEditActivity.java
+++ app/src/main/java/sg/edu/np/mad/dontslack/CalendarEventEditActivity.java
@@ -1,12 +1,15 @@
 package sg.edu.np.mad.dontslack;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.time.LocalTime;
@@ -28,6 +31,18 @@ public class CalendarEventEditActivity extends AppCompatActivity
         time = LocalTime.now();
         eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
         //eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+
+        /* Hiding the top bar */
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+        //back button
+        ImageView backHomePage = findViewById(R.id.backHome);
+        backHomePage.setOnClickListener(v -> {
+            Intent myIntent = new Intent(CalendarEventEditActivity.this, HomePage.class);
+            startActivity(myIntent);
+        });
     }
 
     private void initWidgets()
